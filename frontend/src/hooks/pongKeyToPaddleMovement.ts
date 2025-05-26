e9dd3a0e44f5c4f0d20f1fe71c43286fd65186d8
ldalmass <ldalmass@student.42nice.fr>
2025-05-26 19:13:40 +0200

    ldalmass: MAJOR_REMOTE: Fixed player input not being registered with the gamemodes. Added AI computer to play against.

--- frontend/src/hooks/pongKeyToPaddleMovement.ts
+++ frontend/src/hooks/pongKeyToPaddleMovement.ts
@@ -5,81 +5,90 @@ import * as baby from '@/libs/babylonLibs';
 import * as game from '@/libs/pongLibs';
 
 // Move paddle Up
-export const	movePaddleUp = (pong: game.pongStruct, paddleZ: number): number =>
+export const	movePaddleUp = (pong: React.RefObject<game.pongStruct>, paddleZ: number): number =>
 {
-	if (!pong.paddle1 ) return(0);
+	if (!pong.current.paddle1 ) return(0);
 	return(
 		Math.max
 		(
-			- pong.arenaHeight + (pong.paddleHeight / 2),
+			- pong.current.arenaHeight + (pong.current.paddleHeight / 2),
 			Math.min
 			(
-				pong.arenaHeight - (pong.paddleHeight / 2),
-				paddleZ - pong.paddleSpeed
+				pong.current.arenaHeight - (pong.current.paddleHeight / 2),
+				paddleZ - pong.current.paddleSpeed
 			)
 		)
 	)
 }
 
 // Move paddle Down
-export const	movePaddleDown = (pong: game.pongStruct, paddleZ: number): number =>
+export const	movePaddleDown = (pong: React.RefObject<game.pongStruct>, paddleZ: number): number =>
 {
-	if (!pong.paddle2 ) return(0);
+	if (!pong.current.paddle2 ) return(0);
 	return(
 		Math.max
 		(
-			- pong.arenaHeight + (pong.paddleHeight / 2),
+			- pong.current.arenaHeight + (pong.current.paddleHeight / 2),
 			Math.min
 			(
-				pong.arenaHeight - (pong.paddleHeight / 2),
-				paddleZ + pong.paddleSpeed
+				pong.current.arenaHeight - (pong.current.paddleHeight / 2),
+				paddleZ + pong.current.paddleSpeed
 			)
 		)
 	)
 }
 
-export	const	doPaddleMovement = (pong: game.pongStruct, gamemode: game.gameModes): void =>
+export const	AIMovePaddle = (pong: React.RefObject<game.pongStruct>): void =>
 {
-	if (!pong.paddle1 || !pong.paddle2) return;
-	if (gamemode === game.gameModes.local)
+	if (!pong.current.paddle1 || !pong.current.paddle2 || !pong.current.ball) return;
+
+	if (pong.current.ballDirection.x < 0)	// Ball is going to the AI paddle
 	{
-		if (pong.pressedKeys.has('arrowup'))
-		{
-			pong.paddle1.position.z = movePaddleUp(pong, pong.paddle1.position.z);
-		}
-		if (pong.pressedKeys.has('arrowdown'))
-		{
-			pong.paddle1.position.z = movePaddleDown(pong, pong.paddle1.position.z);
-		}
+		if (pong.current.ball.position.z + pong.current.paddleHeight / 2 < pong.current.paddle1.position.z) pong.current.paddle1.position.z = movePaddleUp(pong, pong.current.paddle1.position.z);
+		if (pong.current.ball.position.z - pong.current.paddleHeight / 2 > pong.current.paddle1.position.z) pong.current.paddle1.position.z = movePaddleDown(pong, pong.current.paddle1.position.z);
 	}
-	if (pong.pressedKeys.has('w'))
+	else	// Ball is going to the player paddle
 	{
-		pong.paddle2.position.z = movePaddleUp(pong, pong.paddle2.position.z);
+		console.log("ball moving away from AI paddle");
+		// Re-center the AI paddle in the arena
+		if (pong.current.paddle1.position.z > 0.2) pong.current.paddle1.position.z = movePaddleUp(pong, pong.current.paddle1.position.z);
+		else if (pong.current.paddle1.position.z < -0.2) pong.current.paddle1.position.z = movePaddleDown(pong, pong.current.paddle1.position.z);
 	}
-	if (pong.pressedKeys.has('s'))
-	{
-		pong.paddle2.position.z = movePaddleDown(pong, pong.paddle2.position.z);
-	}
-	// console.log("Pressed keys: ", pong.pressedKeys);
-	// console.log("Paddle2 position: ", pong.paddle2.position.z);
 }
 
-export const	manageLocalKeyboardInputs = (pong: game.pongStruct, gamemode: game.gameModes): void =>
+export	const	doPaddleMovement = (pong: React.RefObject<game.pongStruct>, gamemode: React.RefObject<game.gameModes>): void =>
+{
+	if (!pong.current.paddle1 || !pong.current.paddle2) return;
+	// console.log("gamemode: ", gamemode.current);
+	switch (gamemode.current)
+	{
+		case game.gameModes.local:
+			if (pong.current.pressedKeys.has('arrowup')) pong.current.paddle1.position.z = movePaddleUp(pong, pong.current.paddle1.position.z);
+			if (pong.current.pressedKeys.has('arrowdown')) pong.current.paddle1.position.z = movePaddleDown(pong, pong.current.paddle1.position.z);
+			break;
+		case game.gameModes.ai:
+			// setTimeout(() => {}, 200);	 // Simulate AI reaction time
+			AIMovePaddle(pong);
+	}
+	if (pong.current.pressedKeys.has('w')) pong.current.paddle2.position.z = movePaddleUp(pong, pong.current.paddle2.position.z);
+	if (pong.current.pressedKeys.has('s')) pong.current.paddle2.position.z = movePaddleDown(pong, pong.current.paddle2.position.z);
+	// console.log("Pressed keys: ", pong.current.pressedKeys);
+}
+
+export const	manageLocalKeyboardInputs = (pong: game.pongStruct): void =>
 {
 	// Keyboard input
-	if (gamemode === game.gameModes.local)
+
+	pong.scene?.onKeyboardObservable.add((kbInfo) =>
 	{
-		pong.scene?.onKeyboardObservable.add((kbInfo) =>
+		const	key = kbInfo.event.key.toLowerCase();
+		if (kbInfo.type === baby.KeyboardEventTypes.KEYDOWN)
 		{
-			const	key = kbInfo.event.key.toLowerCase();
-			if (kbInfo.type === baby.KeyboardEventTypes.KEYDOWN)
-			{
-				pong.pressedKeys.add(key);
-			}
-			else if (kbInfo.type === baby.KeyboardEventTypes.KEYUP)
-			{
-				pong.pressedKeys.delete(key);
-			}
-		});
-	}
+			pong.pressedKeys.add(key);
+		}
+		else if (kbInfo.type === baby.KeyboardEventTypes.KEYUP)
+		{
+			pong.pressedKeys.delete(key);
+		}
+	});
 }
