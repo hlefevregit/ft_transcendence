6b19c56e1b6ca37198798b6d9dce74e59f9d0aee
Jean-Loup Demers <jdemers@student.42nice.fr>
2025-06-06 18:44:13 +0200

    jdemers: made cells clickable + added comments

--- frontend/src/assets/BattleshipMesh.ts
+++ frontend/src/assets/BattleshipMesh.ts
@@ -1,35 +1,46 @@
-import 'babylonjs'
+import BABYLON, { Vector3, MeshBuilder, ActionManager, ActionEvent } from 'babylonjs'
 
+// Custom mesh encapsulating one Battleship player interface
 class BattleshipMesh extends BABYLON.Mesh {
     pivot: BABYLON.Mesh
     screen: BABYLON.Mesh
     field: BABYLON.Mesh
     cells: BABYLON.Mesh[] = []
 
-    constructor( name: string, scene: BABYLON.Scene, position?: BABYLON.Vector3, rotation?: BABYLON.Vector3) {
+    constructor(name: string, scene: BABYLON.Scene, onClick: (ij:number, evt:ActionEvent) => void, position?: Vector3, rotation?: Vector3) {
         super(name, scene);
-        this.pivot = BABYLON.MeshBuilder.CreateCylinder("pivot" + name, {height:10.5, diameter:1.7}, scene);
+
+		// Cylinder pivot for the screen's rotating animation. Mostly aesthetic
+        this.pivot = MeshBuilder.CreateCylinder("pivot-" + name, {height:10.5, diameter:1.7}, scene);
         this.pivot.parent = this;
         this.pivot.rotation.z += Math.PI/2;
 
-        this.screen = BABYLON.MeshBuilder.CreateBox("screen" + name, {width:10, height:10.5, depth:0.7}, scene);
-		this.screen.position = new BABYLON.Vector3(0, 5.6, 0.3);
+		// Upper box, represents the opponent's field. Parent mesh of the cells (see below)
+        this.screen = MeshBuilder.CreateBox("screen-" + name, {width:10, height:10.5, depth:0.7}, scene);
+		this.screen.position = new Vector3(0, 5.6, 0.3);
 
+		// Array of 'cells', small clickable meshes used by player to select where to attack next
         const cellColor = new BABYLON.Color4(0, 0.27, 1);
         for (let i=0; i < 10; i++) {
             for (let j=0; j < 10; j++) {
-                const cell = BABYLON.MeshBuilder.CreateBox("cell" + i + j + name, {size:0.8, depth:0.3, faceColors:Array(6).fill(cellColor)});
+                const cell = MeshBuilder.CreateBox("cell" + i + j + "-" + name, {size:0.8, depth:0.3, faceColors:Array(6).fill(cellColor)});
                 cell.parent = this.screen;
-                cell.position = new BABYLON.Vector3(i-4.5, j-4.25, -0.3);
+                cell.position = new Vector3(i-4.5, j-4.25, -0.3);
+				cell.actionManager = new ActionManager(scene);
+				cell.actionManager.registerAction(new BABYLON.ExecuteCodeAction(
+					ActionManager.OnPickTrigger,
+					(event) => onClick(i*10 + j, event))
+				);
                 this.cells.push(cell);
             }
         }
         this.screen.setParent(this.pivot);
         this.pivot.rotation.x += Math.PI/12
 
-        this.field = BABYLON.MeshBuilder.CreateBox(name + "Field", {width:10.2, height:0.7, depth:10.7}, scene);
+		// Lower box, represents the player's own field. Might be used for ship placement (TBD)
+        this.field = MeshBuilder.CreateBox("field-" + name, {width:10.2, height:0.7, depth:10.7}, scene);
         this.field.setParent(this);
-        this.field.position.addInPlace(new BABYLON.Vector3(0, -0.2, -5.6));
+        this.field.position.addInPlace(new Vector3(0, -0.2, -5.6));
 
         if (position !== undefined)
             this.position.addInPlace(position);
