c96ad5b27c2c72e28b6f1f151ab672f7edac0f76
Thundder <evan.morrealee@gmail.com>
2025-06-16 23:38:56 +0200

    fix(LiveChat) : Back Part, finished, need some adjustement but apriori functionnal

--- frontend/src/components/LiveChat/ChatLauncher.tsx
+++ frontend/src/components/LiveChat/ChatLauncher.tsx
@@ -1,32 +1,17 @@
 // src/components/LiveChat/ChatLauncher.tsx
-import { useState } from 'react'
 import { MdMessage } from 'react-icons/md'
 import ChatPanel from './ChatPanel'
-import type { ChatUser } from '../../types'        // ← ajouté
 import '../../styles/LiveChat/ChatLauncher.css'
 import { useChatStore } from './ChatContext'
 
 export default function ChatLauncher() {
-  const [open, setOpen] = useState(false)
-  const { recentContacts, setRecentContacts } = useChatStore()
-
-  const handleSelect = (user: ChatUser) => {
-    if (!recentContacts.find(u => u.id === user.id)) {
-      setRecentContacts([...recentContacts, user])
-    }
-    setOpen(true)
-  }
+  const { open, setOpen } = useChatStore()
 
   return (
     <>
-      {open && (
-        <ChatPanel
-          contacts={recentContacts}
-          onSelect={handleSelect}
-          onClose={() => setOpen(false)}
-        />
-      )}
-      {!open && (
+      {open ? (
+        <ChatPanel onClose={() => setOpen(false)} />
+      ) : (
         <button
           onClick={() => setOpen(true)}
           className="chat-launcher-button"
