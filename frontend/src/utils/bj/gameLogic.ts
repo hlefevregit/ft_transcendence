1770a164e759099cfb3f52ecc3f36e447ce9e700
lpolizzi <lpolizzi@student.42nice.fr>
2025-06-24 19:20:05 +0200

    fix(card mesh isolation): lpolizzi: Founda way to isolate specific card meshes to handle each card as it's own.

--- frontend/src/utils/bj/gameLogic.ts
+++ frontend/src/utils/bj/gameLogic.ts
@@ -7,16 +7,3 @@ export const PlayGame = (
   updateState: (newState: game.States) => void
 ): void => {;
 };
-
-// Debug function to rotate the camera
-// const rotateCamera = async (
-// 	bjRef: React.RefObject<game.bjStruct>,
-// 	updateState: (newState: game.States) => void
-// ): Promise<void> => {;
-// 	for (let i = 0.01; i < 10; i += 0.01)
-// 	{
-// 		bjRef.current.scene.activeCamera.rotation = new baby.Vector3(0, i, 0);
-// 		console.log("Camera rotation:", bjRef.current.scene.activeCamera.rotation.x, bjRef.current.scene.activeCamera.rotation.y, bjRef.current.scene.activeCamera.rotation.z);
-// 		await new Promise(resolve => setTimeout(resolve, 25)); // Wait for 10ms
-// 	}
-// };
